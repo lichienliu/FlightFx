e1742e1826d137884ddfa44c00922a2aa6e1a8f8
MichaelLiu <llc890410@gmail.com>
2026-06-11 20:17:43 +0800

    feature: 匯率 Repository 實作(啟動抓一次 + 快取)+ Hilt 接線

--- app/src/main/java/com/michaelliu/flightfx/di/CurrencyModule.kt
+++ app/src/main/java/com/michaelliu/flightfx/di/CurrencyModule.kt
@@ -1,6 +1,9 @@
 package com.michaelliu.flightfx.di
 
 import com.michaelliu.flightfx.data.remote.api.CurrencyApi
+import com.michaelliu.flightfx.data.repository.CurrencyRepositoryImpl
+import com.michaelliu.flightfx.domain.repository.CurrencyRepository
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -10,10 +13,16 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object CurrencyModule {
+abstract class CurrencyModule {
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideCurrencyApi(@CurrencyRetrofit retrofit: Retrofit): CurrencyApi =
-        retrofit.create(CurrencyApi::class.java)
+    abstract fun bindCurrencyRepository(impl: CurrencyRepositoryImpl): CurrencyRepository
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideCurrencyApi(@CurrencyRetrofit retrofit: Retrofit): CurrencyApi =
+            retrofit.create(CurrencyApi::class.java)
+    }
 }
